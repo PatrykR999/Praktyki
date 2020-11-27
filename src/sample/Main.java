4b69f8014d3fd1602f29c1fa5c7e0a59d924edae
PatrykR999 <Patryk.rostankowski@psr.gda.pl>
2020-11-27 14:00:07 +0100

    First commit

---
+++ src/sample/Main.java
@@ -0,0 +1,15 @@
+ class MainClass {
+    public static void main (String[] args){
+
+     int liczby [] = {1,2,3,4,5,6,7,8,9,10};
+     int suma = 0;
+     for (int i = 0; i<liczby.length; i++) {
+         suma+=liczby[i];
+     }
+     System.out.println("suma wszystkich liczb wynosi:" + suma);
+
+
+
+
+    }
+ }
